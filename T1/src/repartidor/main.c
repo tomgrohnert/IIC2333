9ea7d79bea7b1036c165911bbc77efb5095ea51b
ftrejoc <ftrejo@uc.cl>
2021-09-02 18:39:13 -0400

    changes in counter

--- T1/src/repartidor/main.c
+++ T1/src/repartidor/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include "./repartidor.h"
 
 void resultados(FILE* output_file, Repartidor* repartidor)
@@ -8,13 +9,24 @@ void resultados(FILE* output_file, Repartidor* repartidor)
   fprintf(output_file,"%d,%d,%d,%d\n",repartidor->t1,repartidor->t2,repartidor->t3,repartidor->t_bodega);
 }
 
-void avanzar(Repartidor* repartidor)
+void avanzar(Repartidor* repartidor, int finish_position)
 {
-  for (int i=0;i<15;i++) // Esto tiene que ser hasta que el repartidor llegue a la bodega
+  bool boolean = true;
+  while (boolean) // Esto tiene que ser hasta que el repartidor llegue a la bodega
   {
-    repartidor->posicion_actual = i;
-    printf("Repartidor %d: Posicion %d\n", repartidor->id, repartidor->posicion_actual);
-    sleep(1);
+    int *current_position = malloc(sizeof(int));
+    *current_position = repartidor->posicion_actual;
+    repartidor->posicion_actual = *current_position + 1;
+    free(current_position);
+    if (repartidor->posicion_actual == finish_position)
+    {
+      printf("Repartidor %d llego: Posicion %d\n", repartidor->id, repartidor->posicion_actual);
+      boolean = false;
+    } else
+    {
+      printf("Repartidor %d: Posicion %d\n", repartidor->id, repartidor->posicion_actual);
+      sleep(1);
+    }
   }
 }
 
@@ -23,5 +35,5 @@ int main(int argc, char const *argv[])
   printf("I'm the REPARTIDOR process and my PID is: %i\n", getpid());
   Repartidor* repartidor = malloc(sizeof(Repartidor));
   repartidor->id = getpid();
-  avanzar(repartidor);
+  avanzar(repartidor, 10);
 }
