8fe768f7a514c6dff714a4a731cd3e626206dbcc
tomgrohnert <tpgrohnert@uc.cl>
2021-09-05 17:35:08 -0300

    more changes

--- T1/src/dccuber/main.c
+++ T1/src/dccuber/main.c
@@ -135,6 +135,7 @@ int main(int argc, char const *argv[])
         all_lights[0] = traffic_light_pid;
         // // Traffic Light 2
         traffic_light_pid = fork();
+        all_lights[0] = traffic_light_pid;
         if (traffic_light_pid == 0)
         {
           char *const argv[] = {"1", data_in->lines[1][3], text_2, NULL}; 
@@ -144,6 +145,7 @@ int main(int argc, char const *argv[])
           // Traffic Light 3
           all_lights[1] = traffic_light_pid;
           traffic_light_pid = fork();
+          all_lights[1] = traffic_light_pid;
           if (traffic_light_pid == 0)
           {
             char *const argv[] = {"2", data_in->lines[1][4], text_2, NULL}; 
@@ -153,6 +155,7 @@ int main(int argc, char const *argv[])
           {
             all_lights[2] = traffic_light_pid;
             // main proccess
+            all_lights[2] = traffic_light_pid;
             printf("MAIN: process %d\n", getpid());
 
             signal(SIGINT, handle_sigint);
@@ -183,6 +186,11 @@ int main(int argc, char const *argv[])
     printf("%s, ", data_in->lines[1][i]);
   }
   printf("\n");
+
+  for (int i=0;i<3;i++)
+  {
+    kill(all_lights[i],SIGINT);
+  }
 
   printf("Liberando memoria...\n");
   input_file_destroy(data_in);
